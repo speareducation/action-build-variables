904bf2737a4c82bb2a3fafeff7ee795cf94891ac
Jason Wright <jwright@speareducation.com>
2021-03-18 09:23:36 -0700

    GL-1839 added projectKey

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
 
 const tag = github.context.ref.replace(/^refs\/(heads|tags)\//, '')
+const projectKey = path.basename(github.context.repository).replace(/\.git$/, '');
+
 const [ text1, appEnv, release ] = tag.split('/');
-console.log({ tag, appEnv, release })
+console.log({ tag, appEnv, release, projectKey })
 
 
 if (!/^(sandbox|dotco|uat|staging|production)$/.test(appEnv)) {
@@ -13,5 +16,6 @@ if (!release) {
     core.setFailed('release could not be parsed from the ref.');
 }
 
+core.setOutput('projectKey', projectKey);
 core.setOutput('appEnv', appEnv);
 core.setOutput('release', release);
