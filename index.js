868049315dc1e883083e7ee94ee20e23b7643cea
John Hutchison <jhutchison@speareducation.com>
2021-07-15 12:03:49 -0700

    Make appEnv and release optional

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const fs = require('fs');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const tag = github.context.ref.replace(/^refs\/(heads|tags)\//, '')
 const projectKey = github.context.repo.repo
+
+const tag = github.context.ref.replace(/^refs\/(heads|tags)\//, '')
 const [ text1, appEnv, release ] = tag.split('/');
 
-let nodeVersion = '';
+if (!/^(sandbox|dotco|uat|staging|production)$/.test(appEnv)) {
+    console.warn('appEnv could not be parsed from the ref, or it is invalid.')
+}
+if (!release) {
+    console.warn('release could not be parsed from the ref.');
+}
+
+let nodeVersion;
 try {
     nodeVersion = fs.readFileSync('.nvmrc', 'utf8').toString().replace(/\s/g, '');
 } catch (err) {
     console.warn('.nvmrc not present, or version not set');
 }
 
-let packageVersion = '';
+let packageVersion;
 try {
     packageVersion = require('./package.json').version;
 } catch (err) {
@@ -21,13 +29,6 @@ try {
 }
 
 console.log({ tag, appEnv, release, projectKey, nodeVersion, packageVersion })
-
-if (!/^(sandbox|dotco|uat|staging|production)$/.test(appEnv)) {
-    core.setFailed('appEnv could not be parsed from the ref, or it is invalid.')
-}
-if (!release) {
-    core.setFailed('release could not be parsed from the ref.');
-}
 
 core.setOutput('projectKey', projectKey);
 core.setOutput('appEnv', appEnv);
